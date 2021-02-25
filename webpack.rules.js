7c8a4b092d6539e9534d440ea4a5a383c8305ea9
JichouP <akkunn100@gmail.com>
2021-02-25 17:08:39 +0900

    init

---
+++ webpack.rules.js
@@ -0,0 +1,45 @@
+module.exports = [
+  // Add support for native node modules
+  {
+    test: /\.node$/,
+    use: 'node-loader',
+  },
+  {
+    test: /\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      options: {
+        outputAssetBase: 'native_modules',
+      },
+    },
+  },
+  {
+    test: /\.tsx?$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        plugins: [
+          [
+            'module-resolver',
+            {
+              alias: {
+                '@': './src',
+              },
+            },
+          ],
+        ],
+      },
+    },
+  },
+  {
+    test: /\.tsx?$/,
+    exclude: /(node_modules|\.webpack)/,
+    use: {
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true,
+      },
+    },
+  },
+];
