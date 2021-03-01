d1a57366ccdd6c8309a65e113f28b84d5b06dae8
JichouP <akkunn100@gmail.com>
2021-03-01 11:30:58 +0900

    apply nodeIntegration

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -12,6 +12,7 @@ const createWindow = (): void => {
   const mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
+    webPreferences: { nodeIntegration: true },
   });
 
   // and load the index.html of the app.
