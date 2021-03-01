3fce44db66f22905fd0b08dea766e43042a32775
Satoshi Takimoto <wasabi.nvim.st@gmail.com>
2021-03-01 14:55:44 +0900

    Rename

--- src/common/typedIpc.ts
+++ src/common/typedIpc.ts
@@ -6,56 +6,59 @@ import {
   BrowserWindow,
 } from 'electron';
 
-type Commands = {
+type FromRenderer = {
   /* example */
   foo(x: number, y: string): boolean;
 };
-type Events = {
+type FromMain = {
   /* example */
   bar(x: number, y: string): void;
 };
 
 export const ipcRenderer = {
-  invoke<K extends keyof Commands>(
+  invoke<K extends keyof FromRenderer>(
     channel: K,
-    ...args: Parameters<Commands[K]>
-  ): Promise<ReturnType<Commands[K]>> {
+    ...args: Parameters<FromRenderer[K]>
+  ): Promise<ReturnType<FromRenderer[K]>> {
     return originalIpcRenderer.invoke(channel, ...args);
   },
 
-  on<K extends keyof Events>(
+  on<K extends keyof FromMain>(
     channel: K,
-    listener: (event: IpcRendererEvent, ...args: Parameters<Events[K]>) => void
+    listener: (
+      event: IpcRendererEvent,
+      ...args: Parameters<FromMain[K]>
+    ) => void
   ): void {
     originalIpcRenderer.on(
       channel,
       (event: IpcRendererEvent, ...args: unknown[]) => {
-        listener(event, ...(args as Parameters<Events[K]>));
+        listener(event, ...(args as Parameters<FromMain[K]>));
       }
     );
   },
 };
 
 export const ipcMain = {
-  handle<K extends keyof Commands>(
+  handle<K extends keyof FromRenderer>(
     channel: K,
     listener: (
       event: IpcMainInvokeEvent,
-      ...args: Parameters<Commands[K]>
-    ) => Promise<ReturnType<Commands[K]>>
+      ...args: Parameters<FromRenderer[K]>
+    ) => Promise<ReturnType<FromRenderer[K]>>
   ): void {
     originalIpcMain.handle(
       channel,
       (event: IpcMainInvokeEvent, ...args: unknown[]) => {
-        return listener(event, ...(args as Parameters<Commands[K]>));
+        return listener(event, ...(args as Parameters<FromRenderer[K]>));
       }
     );
   },
 
-  send<K extends keyof Events>(
+  send<K extends keyof FromMain>(
     window: BrowserWindow,
     channel: K,
-    ...args: Parameters<Events[K]>
+    ...args: Parameters<FromMain[K]>
   ): void {
     window.webContents.send(channel, ...args);
   },
