2d49dcdc441c13bbe0706f2057161ce3ffefa500
JichouP <akkunn100@gmail.com>
2021-03-12 17:18:54 +0900

    fix example

--- src/index.d.ts
+++ src/index.d.ts
@@ -32,19 +32,19 @@ declare namespace TraPCollection {
     description: string;
     /**
      * @example
-     * "contents/games/UUIDv4/*.exe"
-     * "contents/games/UUIDv4/*.jar"
+     * "games/UUIDv4/*.exe"
+     * "games/UUIDv4/*.jar"
      * "*.trap.games"
      */
     url: string;
     /**
      * @example
-     * "contents/artworks/UUIDv4.png"
+     * "artworks/UUIDv4.png"
      */
     poster: string;
     /**
      * @example
-     * "contents/artworks/UUIDv4.mp4"
+     * "artworks/UUIDv4.mp4"
      */
     video?: string;
   };
