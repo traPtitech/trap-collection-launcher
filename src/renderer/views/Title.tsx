160647974255fc400c3a0090e33ff4b0a2698258
Satoshi Takimoto <wasabi.nvim.st@gmail.com>
2021-03-22 23:38:38 +0900

    Center title

--- src/renderer/views/Title.tsx
+++ src/renderer/views/Title.tsx
@@ -4,46 +4,82 @@ import styled from 'styled-components';
 
 const PageContainer = styled.div`
   max-width: 980px;
+  height: 100vh;
   margin-left: auto;
   margin-right: auto;
   padding-left: 10px;
   padding-right: 10px;
+
+  position: relative;
 `;
 
-const Header = styled.header`
-  padding: 20px 0;
+const TitleContainer = styled.header`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const PageTitle = styled.h1``;
+const Title = styled.h1`
+  font-size: 50px;
+`;
 
-const Content = styled.div``;
+const Version = styled.span`
+  margin-left: 10px;
+  font-size: 32px;
+`;
+
+const StartButton = styled(Link)`
+  text-decoration: none;
+  color: white;
+  font-size: 32px;
+  transition: 100ms transform;
+  opacity: 0.75;
+
+  &:hover {
+    transform: scale(1.1);
+    opacity: 1;
+  }
+`;
+
+const StartButtonIcon = styled.span`
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 10px;
+`;
+
+const StartButtonText = styled.span`
+  margin-right: 10px;
+`;
 
 const Footer = styled.footer`
   position: fixed;
   left: 0;
   bottom: 0;
   width: 100%;
-  padding: 20px 0;
-`;
-
-const FooterLinkContainer = styled.div`
-  float: right;
-  padding-right: 20px;
+  padding: 20px;
+  text-align: right;
 `;
 
 const TitlePage: React.FC = () => {
   return (
     <PageContainer>
-      <Header>
-        <PageTitle>traP Collection</PageTitle>
-      </Header>
-      <Content>
-        <Link to='/game'>Play!</Link>
-      </Content>
+      <TitleContainer>
+        <Title>
+          traP Collection<Version>v3</Version>
+        </Title>
+        <StartButton to='/game'>
+          <StartButtonIcon>
+            <ion-icon name='play' />
+          </StartButtonIcon>
+          <StartButtonText>Start!</StartButtonText>
+        </StartButton>
+      </TitleContainer>
       <Footer>
-        <FooterLinkContainer>
-          <Link to='/setting'>Link to setting</Link>
-        </FooterLinkContainer>
+        <Link to='/setting'>Link to setting</Link>
       </Footer>
     </PageContainer>
   );
