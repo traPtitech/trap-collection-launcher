b270b431dc48dfbbb5f8ea04ec19bee49097de7d
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-12-04 21:42:24 +0900

    change slide num times 3 -> 4

--- src/renderer/components/Slider/index.tsx
+++ src/renderer/components/Slider/index.tsx
@@ -31,14 +31,15 @@ const ImageWrapper = styled.div<ImageWrapperProps>`
 
 export type Props = {
   selected: number;
-  gameInfos?: Array<TraPCollection.GameInfo | undefined>;
+  gameInfos: TraPCollection.GameInfo[];
 };
 
 const computePos = (index: number, len: number) => {
   console.log(index);
-  const right = 4.875 + 15 * (index - len) + (index >= len + 1 ? 12.5 : 0);
-  const hidden = index >= len * 2 + 1 || index <= 1;
-  const edge = index == len ? 25.0 : 12.5;
+  const right =
+    4.875 + 15 * (index - 2 * len) + (index >= 2 * len + 1 ? 12.5 : 0);
+  const hidden = index >= 3 * len || index <= len - 1;
+  const edge = index == 2 * len ? 25.0 : 12.5;
   return {
     bottom: 3.25,
     right,
@@ -48,21 +49,28 @@ const computePos = (index: number, len: number) => {
   };
 };
 
+/**
+ * Returns the remainder of a divided by b.
+ * @param a {number} Dividend
+ * @param b {number} Divisor
+ * @returns {number} The remainder of a divided by b
+ */
 const mod = (a: number, b: number) => {
-  return (a % b) + (a < 0 && a % b !== 0 ? b : 0);
+  const m = a % b;
+  return m + (a < 0 && m !== 0 ? b : 0);
 };
 
 const Slider = ({ selected, gameInfos }: Props) => {
-  const gameInfos3 = gameInfos?.concat(gameInfos?.concat(gameInfos));
-  const listImages = gameInfos3?.map((gameInfo, index) => (
+  const gameInfos4 = [...gameInfos, ...gameInfos, ...gameInfos, ...gameInfos];
+  const listImages = gameInfos4.map((gameInfo, index) => (
     <ImageWrapper
       key={index}
       {...computePos(
-        mod(selected - index + 1, gameInfos3.length),
-        gameInfos?.length || 0
+        mod(selected - index + 1, gameInfos4.length),
+        gameInfos.length
       )}
     >
-      <SliderImage src={gameInfo?.poster} />
+      <SliderImage src={gameInfo.poster} />
     </ImageWrapper>
   ));
   return <BackGround>{listImages}</BackGround>;
