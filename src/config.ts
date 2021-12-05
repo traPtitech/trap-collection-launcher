d29a4f67a5663fe0cad94a037ba2ddb0eda6c210
JichouP <akkunn100@gmail.com>
2021-12-05 16:16:25 +0900

    import package.json

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,11 @@
+// eslint-disable-next-line import/no-unresolved
+import PackageJson from '/package.json';
+
 export const baseUrl = 'https://collection-dev.tokyotech.org/';
 export const version = '';
+export const packageJson: {
+  name: string;
+  productName: string;
+  version: string;
+  description: string;
+} = PackageJson;
