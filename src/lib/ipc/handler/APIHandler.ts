77d11d923e598feeac79c65c18a184aeba3450c8
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-12-20 22:56:37 +0900

    APIHandler.ts throw error when network error

--- src/lib/ipc/handler/APIHandler.ts
+++ src/lib/ipc/handler/APIHandler.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { ipcMain } from '@/common/typedIpc';
 import { postLauncherLogin } from '@/lib/axios';
 import { store } from '@/lib/store';
@@ -11,8 +12,13 @@ export const postLauncherLoginHandler = (): void => {
           store.set('token', data.accessToken);
           return true;
         })
-        .catch(() => {
-          return false;
+        .catch((e) => {
+          if (!e.response) {
+            throw new Error('network error');
+          }
+          if (axios.isAxiosError(e) && e.response) {
+            return false; //ネットワークエラー以外の場合は false を返す
+          }
         });
       return res ?? false;
     }
