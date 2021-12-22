2e836cec39cbd266a1837ca6fc7307cbe6cbb09a
JichouP <akkunn100@gmail.com>
2021-12-22 20:57:03 +0900

    add isKoudaisai

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ import PackageJson from '/package.json';
 
 export const baseUrl = 'https://collection-dev.tokyotech.org/api';
 export const version = '';
+export const isKoudaisai = process.env.KOUDAISAI === 'true';
 
 export const packageJson: {
   name: string;
