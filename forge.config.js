38370ec979518cc0dd4cfc0cfa42e1bf0704574c
JichouP <akkunn100@gmail.com>
2021-12-23 10:42:24 +0900

    migrate to forge.config.js

---
+++ forge.config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  packagerConfig: {
+    icon: './assets/icon.ico',
+    appCopyright: 'Copyright (c) 2021 東京工業大学デジタル創作同好会traP',
+    /**
+     * @see {@url https://www.npmjs.com/package/electron-notarize#method-notarizeopts-promisevoid}
+     */
+    osxNotarize: {},
+    /**
+     * @see {@url https://www.npmjs.com/package/electron-osx-sign#opts---options}
+     */
+    osxSign: {},
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'trap_collection',
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+  ],
+  plugins: [
+    [
+      '@electron-forge/plugin-webpack',
+      {
+        mainConfig: './webpack.main.config.js',
+        renderer: {
+          config: './webpack.renderer.config.js',
+          entryPoints: [
+            {
+              html: './src/renderer/index.html',
+              js: './src/renderer/renderer.tsx',
+              name: 'main_window',
+              preload: {
+                js: './src/preload/preload.ts',
+              },
+            },
+          ],
+        },
+      },
+    ],
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      platforms: ['win32', 'darwin'],
+      config: {
+        repository: {
+          owner: 'traPtitech',
+          name: 'trap-collection-launcher',
+        },
+        draft: false,
+        prerelease: true,
+      },
+    },
+  ],
+};
