92e9a3b16093b7221ec51767d504fad1dd18c97f
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-12-25 21:39:03 +0900

    change type GameInfo.version

--- src/lib/ipc/handler/getGameInfoHandler.ts
+++ src/lib/ipc/handler/getGameInfoHandler.ts
@@ -5,22 +5,24 @@ import { generateAbsolutePath } from '@/lib/utils/generatePaths';
 export const getGameInfoHandler = (): void => {
   ipcMain.handle('getGameInfo', async () => {
     const gameInfo = store.get('gameInfo');
-    return gameInfo.map((v) => {
-      const { poster, video, url } = v;
-      if (v.type === 'url') {
+    return gameInfo
+      .filter((v) => v.version)
+      .map((v) => {
+        const { poster, video, url } = v;
+        if (v.type === 'url') {
+          return {
+            ...v,
+            poster: generateAbsolutePath(poster),
+            video: video ? generateAbsolutePath(video) : undefined,
+          };
+        }
         return {
           ...v,
           poster: generateAbsolutePath(poster),
           video: video ? generateAbsolutePath(video) : undefined,
+          url: generateAbsolutePath(url),
         };
-      }
-      return {
-        ...v,
-        poster: generateAbsolutePath(poster),
-        video: video ? generateAbsolutePath(video) : undefined,
-        url: generateAbsolutePath(url),
-      };
-    });
+      });
   });
 };
 
