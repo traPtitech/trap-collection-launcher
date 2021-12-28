4df4e3788853a891eca42777824886a865ec8203
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-12-28 23:38:26 +0900

    fix setProductKeyHandler

--- src/lib/ipc/handler/setProductKeyHandler.ts
+++ src/lib/ipc/handler/setProductKeyHandler.ts
@@ -3,6 +3,6 @@ import { store } from '@/lib/store';
 
 export const setProductKeyHandler = (): void => {
   ipcMain.handle('setProductKey', async (_, productKey) => {
-    return store.set('launcherVersions', [{ id: productKey }]);
+    return store.set('launcherVersions', [{ productKey: productKey }]);
   });
 };
